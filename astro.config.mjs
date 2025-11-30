d3f257e446c4dc4822858d7601cec14531bfbbb1
JoaoCupido <jcupido13@gmail.com>
2025-11-30 18:31:35 +0000

    oopsies part 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
     build: {
         assetsDir: '_astro',
     },
-    site: 'https://joaocupido.github.io/neuro-exercises',
-    base: isGithubPages ? '/neuro-exercises/' : '/',
+    site: 'https://joaocupido.github.io',
+    base: isGithubPages ? '/neuro-exercises' : '/',
 });
